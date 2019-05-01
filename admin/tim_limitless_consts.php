d22ac1fd4247d7fef077dd09d1f17cb3959c28bc
shlomisut <shlomi@memeglobal.com>
2019-05-01 15:28:50 +0300

    Moving URLs to audio.trinityaudio.ai

--- admin/tim_limitless_consts.php
+++ admin/tim_limitless_consts.php
@@ -10,16 +10,19 @@ const TIM_LIMITLESS_POST_HASH_CONTENT_EXCERPT = 'tim_limitless_post_hash_content
 const TIM_LIMITLESS_POST_HASH_CONTENT_EXCERPT_TITLE = 'tim_limitless_post_hash_content_excerpt_title';
 const TIM_LIMITLESS_STARTUP = "https://trinitymedia.ai/sas/player/amazon_plugin/startup.php";
 const TIM_LIMITLESS_AUDIO_SILENCE = "https://vd.trinitymedia.ai/loading.mp3";
-const TIM_LIMITLESS_POST_HASH_URL = 'https://audio.thetimmedia.com/wordpress/get_posthash';
-const TIM_LIMITLESS_KEYS_URL = 'http://r2v2.memeglobal.com/api/wordpress_campaign?site_domain=';
-const TIM_LIMITLESS_AUDIO_URL = 'https://audio.thetimmedia.com/process';
+
+const TRINITY_AUDIO_SERVICE = 'https://audio.trinityaudio.ai/';
+const TIM_LIMITLESS_POST_HASH_URL = TRINITY_AUDIO_SERVICE . 'wordpress/get_posthash';
+const TIM_LIMITLESS_KEYS_URL = TRINITY_AUDIO_SERVICE . 'wordpress/signup?site_domain=';
+const TIM_LIMITLESS_LANGUAGES = TRINITY_AUDIO_SERVICE . "wordpress/get_languages?installkey=";
+const TIM_LIMITLESS_AUDIO_URL = TRINITY_AUDIO_SERVICE . 'process';
+
 const AMAZON_POLLY_ACCESS_KEY_DIV = 'amazon_polly_access_key_div';
 const AMAZON_POLLY_SECRET_KEY_DIV = 'amazon_polly_secret_key_div';
 const AMAZON_POLLY_ACCESS_KEY = 'amazon_polly_access_key';
 const AMAZON_POLLY_SECRET_KEY = 'amazon_polly_secret_key';
 const TIM_LIMITLESS_KEYS_ERROR_MESSAGE = "Can't connect to tim limitless! Please contact Tim Support support@thetimmedia.com";
 const TIM_LIMITLESS_GENDER_ID = 'tim_limitless_gender_id';
-const TIM_LIMITLESS_LANGUAGES = "https://audio.thetimmedia.com/wordpress/get_languages?installkey=";
 
 const GENDER_LABEL_FEMALE = 'Female';
 const GENDER_LABEL_MALE = 'Male';
